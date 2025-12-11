3ddab56c01b5471fdff19e66bc6271a6031fd12d
Manikandan <manikandan.thedev@gmail.com>
2025-12-11 21:51:05 +0530

    db.js fixed

--- db.js
+++ db.js
@@ -4,7 +4,6 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  port: process.env.DB_PORT,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT) || 3306,
