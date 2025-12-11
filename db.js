01c17b759ca297a8b02a9b16569b30e17bf16e93
Manikandan <manikandan.thedev@gmail.com>
2025-12-11 21:47:09 +0530

    db.js error fixed

--- db.js
+++ db.js
@@ -4,11 +4,23 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+(async () => {
+  try {
+    const conn = await pool.getConnection();
+    console.log("DB connected successfully");
+    conn.release();
+  } catch (err) {
+    console.error("DB connection failed:", err.message);
+  }
+})();
 
 module.exports = pool;
